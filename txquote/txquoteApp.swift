5f0d76b9b67fe31d6d47a80ec44b2ec7dd921ede
Tung Doan <tungdoan@emddi.com>
2024-11-05 14:57:18 +0700

    Initial Commit

---
+++ txquote/txquoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  txquoteApp.swift
+//  txquote
+//
+//  Created by Tùng Đoàn on 05/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct txquoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
