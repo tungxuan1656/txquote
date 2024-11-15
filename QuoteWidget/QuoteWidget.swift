2441aeb9459b8c99d1745f7747b4ba93fd343917
Tung Doan <tungdoan@emddi.com>
2024-11-15 14:44:37 +0700

    random quote

--- QuoteWidget/QuoteWidget.swift
+++ QuoteWidget/QuoteWidget.swift
@@ -9,69 +9,70 @@ import WidgetKit
 import SwiftUI
 
 struct Provider: TimelineProvider {
-  
-  let placeholderEntry = QuoteEntry(date: Date(), text: "Tâm lý", link: "txquote://")
-  
-  func placeholder(in context: Context) -> QuoteEntry {
-    return placeholderEntry
-  }
-  
-  func getSnapshot(in context: Context, completion: @escaping (QuoteEntry) -> ()) {
-    completion(placeholderEntry)
-  }
-  
-  func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-    var entries: [QuoteEntry] = []
-    let listQuotes = loadQuoteJson()
     
-    // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-    let currentDate = Date()
-    for i in 0...10 {
-      let quote = listQuotes.randomElement()
-      let date = Calendar.current.date(byAdding: .minute, value: i * 10, to: currentDate)!
-      let entry = QuoteEntry(date: date, text: quote?.text ?? "#", link: quote?.link ?? "txquote://")
-      entries.append(entry)
+    let placeholderEntry = QuoteEntry(date: Date(), text: "Tâm lý", link: "txquote://")
+    
+    func placeholder(in context: Context) -> QuoteEntry {
+        return placeholderEntry
     }
     
-    let timeline = Timeline(entries: entries, policy: .atEnd)
-    completion(timeline)
-  }
+    func getSnapshot(in context: Context, completion: @escaping (QuoteEntry) -> ()) {
+        completion(placeholderEntry)
+    }
+    
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [QuoteEntry] = []
+        let listQuotes = loadQuoteJson()
+        
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        let shuffle = listQuotes.shuffled()
+        for i in 0...shuffle.count {
+            let quote = shuffle[i]
+            let date = Calendar.current.date(byAdding: .minute, value: i * 3, to: currentDate)!
+            let entry = QuoteEntry(date: date, text: quote.text, link: quote.link)
+            entries.append(entry)
+        }
+        
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
 }
 
 struct QuoteEntry: TimelineEntry {
-  let date: Date
-  let text: String
-  let link: String
+    let date: Date
+    let text: String
+    let link: String
 }
 
 struct QuoteWidgetEntryView : View {
-  var entry: Provider.Entry
-  
-  var body: some View {
-    Link(destination: URL(string: entry.link)!) {
-      VStack {
-        Text("Jess Tản Mạn").font(.headline).padding(.bottom, 16).foregroundColor(.white)
-        Text(entry.text).font(.caption).lineSpacing(5).foregroundColor(.white)
-      }
+    var entry: Provider.Entry
+    
+    var body: some View {
+        Link(destination: URL(string: entry.link)!) {
+            VStack {
+                Text("Jess Tản Mạn").font(.headline).padding(.bottom, 16).foregroundColor(.white)
+                Text(entry.text).font(.caption).lineSpacing(5).foregroundColor(.white)
+            }
+        }
     }
-  }
 }
 
 struct QuoteWidget: Widget {
-  let kind: String = "QuoteWidget"
-  
-  var body: some WidgetConfiguration {
-    StaticConfiguration(kind: kind, provider: Provider()) { entry in
-      if #available(iOS 17.0, *) {
-        QuoteWidgetEntryView(entry: entry)
-          .containerBackground(Color(red: 32/255, green: 33/255, blue: 36/255), for: .widget)
-      } else {
-        QuoteWidgetEntryView(entry: entry)
-          .padding()
-          .background().foregroundColor(Color(red: 32/255, green: 33/255, blue: 36/255))
-      }
+    let kind: String = "QuoteWidget"
+    
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            if #available(iOS 17.0, *) {
+                QuoteWidgetEntryView(entry: entry)
+                    .containerBackground(Color(red: 32/255, green: 33/255, blue: 36/255), for: .widget)
+            } else {
+                QuoteWidgetEntryView(entry: entry)
+                    .padding()
+                    .background().foregroundColor(Color(red: 32/255, green: 33/255, blue: 36/255))
+            }
+        }
+        .configurationDisplayName("Trading")
+        .description("Các chia sẻ được lấy từ Jess Tản Mạn của Thầy Zet Under. Tất cả điều hoàn toàn free")
     }
-    .configurationDisplayName("Trading")
-    .description("Các chia sẻ được lấy từ Jess Tản Mạn của Thầy Zet Under. Tất cả điều hoàn toàn free")
-  }
 }
